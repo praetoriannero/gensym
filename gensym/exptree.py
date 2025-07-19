2dd13f4dbea2c3ad88e309383f57b9b8076112d0
Nicholas Heaven <nick.heaven0930@gmail.com>
2025-07-18 23:29:01 -0500

    added pyproject

--- gensym/exptree.py
+++ gensym/exptree.py
@@ -1,10 +1,7 @@
 from abc import ABC
-import matplotlib.pyplot as plt
 import networkx as nx
 import numpy as np
 import random
-from sklearn.datasets import make_regression
-from sklearn.preprocessing import MinMaxScaler
 from typing import Any
 
 
@@ -27,7 +24,7 @@ class Leaf(Node):
 
 
 class Operator(Node):
-    def __init__(self, op: str | None=None, **kwargs):
+    def __init__(self, op: str | None = None, **kwargs):
         super().__init__(**kwargs)
         if op is None:
             self._op = random.choice(list(self.dispatch))
@@ -42,6 +39,7 @@ class BinaryOperator(Operator):
     BinaryOperator can represent one of the following:
     +, -, *, /
     """
+
     dispatch = {
         "+": lambda x, y: x + y,
         "-": lambda x, y: x - y,
@@ -55,7 +53,11 @@ class BinaryOperator(Operator):
 
     def to_string(self, graph: nx.DiGraph) -> str:
         child_a, child_b = list(graph.successors(self))
-        return str(child_a.to_string(graph)) + f" {self._op} " + str(child_b.to_string(graph))
+        return (
+            str(child_a.to_string(graph))
+            + f" {self._op} "
+            + str(child_b.to_string(graph))
+        )
 
 
 class UnaryOperator(Operator):
@@ -63,6 +65,7 @@ class UnaryOperator(Operator):
     UnaryOperator can represent one of the following:
     sin, cos, tan, inv
     """
+
     dispatch = {
         "sin": lambda x: np.sin(x),
         "cos": lambda x: np.cos(x),
@@ -83,16 +86,19 @@ class Variable(Leaf):
     """
     Variable takes in a matrix and selects one of the columns in the matrix.
     """
+
     child_count = 0
 
-    def __init__(self, data: np.ndarray, column: int | None=None, **kwargs):
+    def __init__(self, data: np.ndarray, column: int | None = None, **kwargs):
         self.data = data
-        self.col = column if column is not None else np.random.randint(0, data.shape[-1])
+        self.col = (
+            column if column is not None else np.random.randint(0, data.shape[-1])
+        )
         super().__init__(**kwargs)
 
     def forward(self) -> np.ndarray:
         return self.data[:, self.col]
-    
+
     def to_string(self, *args) -> str:
         return f"x_{self.col}"
 
@@ -101,9 +107,10 @@ class Value(Leaf):
     """
     Value represents some floating point number that can mutate.
     """
+
     child_count = 0
 
-    def __init__(self, init_value: float | int | None=None, **kwargs):
+    def __init__(self, init_value: float | int | None = None, **kwargs):
         self.value = (np.random.rand() - 1) * 2.0 if init_value is None else init_value
         super().__init__(**kwargs)
 
@@ -118,7 +125,8 @@ class ExpressionTree:
     """
     ExpressionTree represents the tree of expressions executed in topological ordering.
     """
-    def __init__(self, data: np.ndarray, init_depth: int=3, max_depth: int=100):
+
+    def __init__(self, data: np.ndarray, init_depth: int = 3, max_depth: int = 100):
         self.data = data
         self.init_depth = init_depth
         self.max_depth = max_depth
@@ -129,7 +137,7 @@ class ExpressionTree:
 
     def _gather_sinks(self) -> list[Node]:
         return [node for node, out_degree in self.graph.out_degree() if not out_degree]
-    
+
     def _grow(self, node: Node, depth: int) -> None:
         if not isinstance(node, Operator):
             return
@@ -140,7 +148,9 @@ class ExpressionTree:
             node_types = self.node_types
 
         depth += 1
-        child_nodes = [random.choice(node_types)(data=self.data) for _ in range(node.child_count)]
+        child_nodes = [
+            random.choice(node_types)(data=self.data) for _ in range(node.child_count)
+        ]
         for child in child_nodes:
             self.graph.add_edge(node, child)
             self._grow(child, depth)
--- gensym/main.py
+++ gensym/main.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import numpy as np
 from sklearn.datasets import make_regression
 from sklearn.preprocessing import MinMaxScaler
 
