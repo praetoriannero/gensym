1d5778185fa5a8bf71eb2bbb60efcc1f099fc41a
Nicholas Heaven <nick.heaven0930@gmail.com>
2025-08-03 16:23:06 -0500

    adding to base tree

--- gensym/_base_tree.py
+++ gensym/_base_tree.py
@@ -5,14 +5,31 @@ import numpy as np
 
 
 class BaseTree(ABC):
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        min_depth: int = 1,
+        max_depth: int = 4,
+        crossover_prob: float = 0.5,
+        branch_mutation_prob: float = 0.5,
+        node_mutation_prob: float = 0.5,
+        hoist_mutation_prob: float = 0.5,
+        tree_simplify_prob: float = 0.5,
+        optimize_const_prob: float = 0.5,
+    ):
+        self.min_depth = min_depth
+        self.max_depth = max_depth
+        self.crossover_prob = crossover_prob
+        self.branch_mutation_prob = branch_mutation_prob
+        self.node_mutation_prob = node_mutation_prob
+        self.hoist_mutation_prob = hoist_mutation_prob
+        self.tree_simplify_prob = tree_simplify_prob
+        self.optimize_const_prob = optimize_const_prob
 
     def fit(self, inputs: np.ndarray, target: np.ndarray) -> BaseTree:
-        pass
+        raise NotImplementedError
 
     def predict(self, inputs: np.ndarray) -> np.ndarray:
-        pass
+        raise NotImplementedError
 
     def fit_predict(self, inputs: np.ndarray, target: np.ndarray) -> np.ndarray:
-        pass
+        raise NotImplementedError
