8f0c8c3d09a23c4a930ba2f380bb1ca4ca1bd769
Nicholas Heaven <nick.heaven0930@gmail.com>
2025-08-03 12:32:56 -0500

    added base classifier

---
+++ gensym/classification.py
@@ -0,0 +1,17 @@
+from gensym.base_tree import BaseTree
+
+import numpy as np
+
+
+class Classifier(BaseTree):
+    def __init__(self):
+        pass
+
+    def fit(self, inputs: np.ndarray, target: np.ndarray) -> Classifier:
+        pass
+
+    def predict(self, inputs: np.ndarray) -> np.ndarray:
+        pass
+
+    def fit_predict(self, inputs: np.ndarray, target: np.ndarray) -> np.ndarray:
+        pass
